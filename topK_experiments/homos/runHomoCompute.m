07b07b37a9041537ab7adb2c54657824aedbf1aa
rohit <rohit@atom>
2014-04-07 10:14:57 +0530

    nits

--- topK_experiments/homos/runHomoCompute.m
+++ topK_experiments/homos/runHomoCompute.m
@@ -1,11 +1,12 @@
 function runHomoCompute(img_idx)
-OUTPUT_DIR = '/Pulsar3/rohit.girdhar/topK_experiments/homos/results/';
+OUTPUT_DIR = '/Pulsar3/rohit.girdhar/topK_experiments/homos/all_results/results_bob/';
+IMAGES_DIR = 'Images_bob/';
 OUTF = strcat(OUTPUT_DIR, 'homo_', int2str(img_idx), '.txt');
 delete(OUTF);
 N = 285;
-I = imread(strcat('Images_sc/', sprintf('%08d', img_idx), '.jpg'));
+I = imread(strcat(IMAGES_DIR, sprintf('%08d', img_idx), '.jpg'));
 for j = 0 : N-1
-	P = imread(strcat('Images_sc/', sprintf('%08d', j), '.jpg'));
+	P = imread(strcat(IMAGES_DIR, sprintf('%08d', j), '.jpg'));
 	H = computeHomo(I, P);
 	f = fopen(OUTF, 'a');
 	for row = 1 : 3
